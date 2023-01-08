806b637e3dc7cbf921da27fee22d3378fe5af644
LuquesP <lukas.jo.pilsl@gmail.com>
2023-01-08 01:45:54 +0100

    .

--- led/led.c
+++ led/led.c
@@ -1,13 +1,28 @@
 #include <linux/module.h>
+#include <linux/version.h>
 #include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/device.h>
+#include <linux/cdev.h>
 #include <linux/init.h>
 #include <linux/gpio.h>
 #include <linux/delay.h>
-
+#include <linux/uaccess.h>
+#include <linux/string.h>
+#include <linux/ktime.h>  
 
 
 // resource for device file  
 // https://sysplay.github.io/books/LinuxDrivers/book/Content/Part05.html
+
+static dev_t first; // Global variable for the first device number
+static struct cdev c_dev; // Global variable for the character device structure
+static struct class *cl; // Global variable for the device class
+
+#define DRIVER_NAME "led" 
+
 
 #define ROW_1_GPIO 1
 #define ROW_2_GPIO 2
@@ -27,54 +42,243 @@
 #define COL_7_GPIO 14
 #define COL_8_GPIO 13
 
+
+// cols = [1, 2, 3, 4, 12, 11, 10, 9] 
+// rows = [5, 6, 7, 8, 16, 15, 14, 13]
+
+unsigned int gpio_rows[] = {
+        5, 
+        6, 
+        7, 
+        8, 
+        16, 
+        15, 
+        14, 
+        13, 
+
+}; 
+
+unsigned int gpio_cols[] = {
+        1, 
+        2, 
+        3, 
+        4, 
+        12, 
+        11,
+        10, 
+        9, 
+}; 
+
+static char command_buffer[255]; 
+static char buffer_pointer = 0; 
+
+int clear_matrix[8][8] = {
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+}; 
+int up_matrix[8][8] = {
+    {0, 0, 0, 1, 1, 0, 0, 0},
+    {0, 0, 0, 1, 1, 0, 0, 0},
+    {0, 0, 0, 1, 1, 0, 0, 0},
+    {0, 0, 0, 1, 1, 0, 0, 0},
+    {1, 0, 0, 1, 1, 0, 0, 1},
+    {0, 1, 0, 1, 1, 0, 1, 0},
+    {0, 0, 1, 0, 0, 1, 0, 0},
+    {0, 0, 0, 1, 1, 0, 0, 0},
+};
+
+int down_matrix[8][8] = {
+    {0, 0, 0, 1, 1, 0, 0, 0},
+    {0, 0, 1, 1, 1, 1, 0, 0},
+    {0, 1, 0, 1, 1, 0, 1, 0},
+    {1, 0, 0, 1, 1, 0, 0, 1},
+    {0, 0, 0, 1, 1, 0, 0, 0},
+    {0, 0, 0, 1, 1, 0, 0, 0},
+    {0, 0, 0, 1, 1, 0, 0, 0},
+    {0, 0, 0, 1, 1, 0, 0, 0},
+};
+
+void draw_arrow(void){
+    int matrix[8][8];
+    memcpy(matrix, clear_matrix, sizeof(matrix));  
+    int row, col; 
+    ktime_t endwait;
+    int seconds = 10;   
+    int len_command_buffer = (int)strlen(command_buffer) - 1; 
+    printk("command_buffer: %.*s\n", len_command_buffer, command_buffer); 
+    if (strncmp(command_buffer, "down", len_command_buffer) == 0) {
+        printk("command is down \n"); 
+        // draw_arrow(down_matrix); 
+        memcpy(matrix, down_matrix, sizeof(matrix)); 
+    }
+    if(strncmp(command_buffer, "up", len_command_buffer) == 0){
+        printk("command is up\n"); 
+        // draw_arrow(up_matrix); 
+        memcpy(matrix, up_matrix, sizeof(matrix)); 
+    }
+    endwait = ktime_add_ns(ktime_get(), seconds * NSEC_PER_SEC); 
+    while (ktime_get() < endwait) {
+        for (row = 0; row < 8; row++) {
+            gpio_set_value(gpio_rows[row], 1);
+            for (col = 0; col < 8; col++) {
+                if (matrix[row][col] == 1) {
+                    gpio_set_value(gpio_cols[col], 0);
+                } else {
+                    gpio_set_value(gpio_cols[col], 1);
+                }
+            }
+            gpio_set_value(gpio_rows[row], 0);
+        }
+        // if(strncmp(command_buffer, "exit", len_command_buffer) == 0) 
+        //     break; 
+    }
+}
+static ssize_t my_write(struct file *f, const char *user_buffer, size_t count,
+    loff_t *off)
+{
+    int to_copy, not_copied, delta; 
+
+    printk(KERN_INFO "Driver: write()\n");
+
+    to_copy = min(count, sizeof(command_buffer)); 
+
+    not_copied = copy_from_user(command_buffer, user_buffer, to_copy); 
+    buffer_pointer = to_copy;
+
+    delta = to_copy - not_copied; 
+
+    draw_arrow();
+
+
+    return delta; 
+
+}
+
+static ssize_t my_read(struct file *f, char *user_buffer, size_t count, loff_t *off)
+{
+    int to_copy, not_copied, delta; 
+    
+    printk(KERN_INFO "Driver: read()\n");
+
+    to_copy = min(count, buffer_pointer ); 
+
+    not_copied = copy_to_user(user_buffer, command_buffer, to_copy); 
+
+    delta = to_copy - not_copied;
+
+    return delta;  
+
+
+}
+static int my_open(struct inode *i, struct file *f)
+{
+    printk(KERN_INFO "Driver: open()\n");
+    return 0;
+}
+static int my_close(struct inode *i, struct file *f)
+{
+    printk(KERN_INFO "Driver: close()\n");
+    return 0;
+}
+static struct file_operations pugs_fops =
+{
+    .owner = THIS_MODULE,
+    .open = my_open,
+    .release = my_close,
+    .read = my_read,
+    .write = my_write
+};
+
+
 static int __init led_matrix_init(void)
 {
+    int ret;
+    int i; 
+    struct device *dev_ret;
 
-  int i;
-  int cnt = 0; 
+    printk(KERN_INFO "LED INIT");
 
-  printk(KERN_INFO "Hello, world!\n");
-  // Set all row and column GPIOs as outputs
- // Set all row and column GPIOs as outputs
-  for (i = ROW_1_GPIO; i <= ROW_8_GPIO; i++)
-    gpio_direction_output(i, 0);
-  for (i = COL_1_GPIO; i <= COL_8_GPIO; i++)
-    gpio_direction_output(i, 0);
-
-  // Turn on all LEDs
-  while (1)
-  {
-    for (i = ROW_1_GPIO; i <= ROW_8_GPIO; i++)
-      gpio_set_value(i, 1);
-    for (i = COL_1_GPIO; i <= COL_8_GPIO; i++)
-      gpio_set_value(i, 1);
-    msleep(500);  // Sleep for 500 milliseconds
-    for (i = ROW_1_GPIO; i <= ROW_8_GPIO; i++)
-      gpio_set_value(i, 0);
-    for (i = COL_1_GPIO; i <= COL_8_GPIO; i++)
-      gpio_set_value(i, 0);
-    msleep(500);  // Sleep for 500 milliseconds
-    cnt = cnt + 1; 
-    if (cnt == 5) {
-      break; 
+    // Device nummer festelegen
+    if ((ret = alloc_chrdev_region(&first, 0, 1, "LED")) < 0)
+    {
+        printk("device konnte nicht registriert werden\n"); 
+        return ret;
     }
-  }
+    printk("Device Nr. Major:%d, Minor %d wurde registriert\n", first >> 20, first && first && 0xfffff); 
+
+    // Device Klasse erstellen 
+    if (IS_ERR(cl = class_create(THIS_MODULE, "chardrv")))
+    {   
+        printk("Device class konnte nicht erstellt werden\n"); 
+        unregister_chrdev_region(first, 1);
+        return PTR_ERR(cl);
+    }
+
+    // Device File erstellen 
+    if (IS_ERR(dev_ret = device_create(cl, NULL, first, NULL, DRIVER_NAME)))
+    {
+        printk("device file konnte nicht erstellt werden\n"); 
+        class_destroy(cl);
+        unregister_chrdev_region(first, 1);
+        return PTR_ERR(dev_ret);
+    }
+    // Device File initialisieren 
+    cdev_init(&c_dev, &pugs_fops);
+
+    // Regestrierung Device zum Kernel
+    if ((ret = cdev_add(&c_dev, first, 1)) < 0)
+    {
+        device_destroy(cl, first);
+        class_destroy(cl);
+        unregister_chrdev_region(first, 1);
+        return ret;
+    }
+
+    //init gpios   
+    for(i = 0; i < 8; i++) {
+        if (gpio_request(gpio_rows[i], NULL)){
+            printk("led_driver - Error Init rows GPIO %d\n", gpio_rows[i]);  
+        }
+        if (gpio_request(gpio_cols[i], NULL)){
+            printk("led_driver - Error Init cols GPIO %d\n", gpio_cols[i]); 
+        }
+
+    //setzen der output richtung
+    for(i = 0; i < 8; i++){
+        if (gpio_direction_output(gpio_rows[i], 0)){
+            printk("led_driver Error setzen der row GPIO %d als output\n", i); 
+        }
+        if(gpio_direction_output(gpio_cols[i], 0)){
+            printk("led_driver Error setzen der row GPIO %d als output\n", i); 
+        }
+    }
+    
+    }
 
   return 0;
 }
 
 static void __exit led_matrix_exit(void)
 {
-
+    int i; 
+    for (i=0; i<8; i++){
+        gpio_set_value(gpio_rows[i], 0);  
+        gpio_set_value(gpio_cols[i], 0); 
+        gpio_free(gpio_rows[i]); 
+        gpio_free(gpio_cols[i]); 
+    }
   // Turn off all LEDs before exiting
-  int i;
-
-  for (i = ROW_1_GPIO; i <= ROW_8_GPIO; i++)
-    gpio_set_value(i, 1);
-  for (i = COL_1_GPIO; i <= COL_8_GPIO; i++)
-    gpio_set_value(i, 1);
-
-  printk(KERN_INFO "Goodbye, world!\n");
+  cdev_del(&c_dev);
+  device_destroy(cl, first);
+  class_destroy(cl);
+  unregister_chrdev_region(first, 1);
+  printk(KERN_INFO "Goodbye, Kernel!\n");
 }
 
 module_init(led_matrix_init);
--- led/led.mod.c
+++ led/led.mod.c
@@ -28,15 +28,34 @@ MODULE_INFO(retpoline, "Y");
 static const struct modversion_info ____versions[]
 __used __section("__versions") = {
 	{ 0x90f75b7e, "module_layout" },
-	{ 0xf9a482f9, "msleep" },
+	{ 0xcce0699e, "cdev_del" },
+	{ 0xfe990052, "gpio_free" },
 	{ 0xc29f9195, "gpiod_direction_output_raw" },
-	{ 0x92997ed8, "_printk" },
-	{ 0xb1ad28e0, "__gnu_mcount_nc" },
+	{ 0x47229b5c, "gpio_request" },
+	{ 0x9ab547c5, "class_destroy" },
+	{ 0x97877479, "device_destroy" },
+	{ 0x1d01e681, "cdev_add" },
+	{ 0xf37beb9b, "cdev_init" },
+	{ 0x42d9689d, "device_create" },
+	{ 0x6091b333, "unregister_chrdev_region" },
+	{ 0xcfffe6ec, "__class_create" },
+	{ 0xe3ec2f2b, "alloc_chrdev_region" },
+	{ 0x5f754e5a, "memset" },
+	{ 0xae353d77, "arm_copy_from_user" },
+	{ 0x8f678b07, "__stack_chk_guard" },
+	{ 0x3ea1b6e4, "__stack_chk_fail" },
+	{ 0xb43f9365, "ktime_get" },
+	{ 0x9d669763, "memcpy" },
+	{ 0x84b183ae, "strncmp" },
+	{ 0x97255bdf, "strlen" },
+	{ 0x51a910c0, "arm_copy_to_user" },
 	{ 0xf08551e, "gpiod_set_raw_value" },
 	{ 0x6d44e361, "gpio_to_desc" },
+	{ 0x92997ed8, "_printk" },
+	{ 0xb1ad28e0, "__gnu_mcount_nc" },
 };
 
 MODULE_INFO(depends, "");
 
 
-MODULE_INFO(srcversion, "7F530BB5E1CDA654DCDC1FC");
+MODULE_INFO(srcversion, "B44D84DC4E4001F4C9AE441");
